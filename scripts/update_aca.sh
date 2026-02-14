fb027c7cdbc1fa60cb389ca52b3ee6e513bd9614
Arturo Quiroga <arturoqu@microsoft.com>
2026-02-14 15:56:40 -0500

    Add GPT-5.2 Codex deployment build arg to Dockerfile and deploy script

--- scripts/update_aca.sh
+++ scripts/update_aca.sh
@@ -38,7 +38,8 @@ export $(cat .env | grep -v '^#' | xargs) && az acr build --registry acrazuredia
     --build-arg VITE_AZURE_OPENAI_API_KEY="$VITE_AZURE_OPENAI_API_KEY" \
     --build-arg VITE_AZURE_OPENAI_DEPLOYMENT_GPT52="$VITE_AZURE_OPENAI_DEPLOYMENT_GPT52" \
     --build-arg VITE_AZURE_OPENAI_DEPLOYMENT_GPT41="$VITE_AZURE_OPENAI_DEPLOYMENT_GPT41" \
-    --build-arg VITE_AZURE_OPENAI_DEPLOYMENT_GPT41MINI="$VITE_AZURE_OPENAI_DEPLOYMENT_GPT41MINI" . \
+    --build-arg VITE_AZURE_OPENAI_DEPLOYMENT_GPT41MINI="$VITE_AZURE_OPENAI_DEPLOYMENT_GPT41MINI" \
+    --build-arg VITE_AZURE_OPENAI_DEPLOYMENT_GPT52CODEX="$VITE_AZURE_OPENAI_DEPLOYMENT_GPT52CODEX" . \
 && az containerapp update --name azure-diagram-builder \
     --resource-group azure-diagrams-rg \
     --image acrazurediagrams1767583743.azurecr.io/azure-diagram-builder:latest \
